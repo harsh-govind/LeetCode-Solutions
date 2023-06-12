84ae1fade15079341b51adf1f71414064aadf918
Peng-Yu Chen <me@pengyuc.com>
2023-06-12 02:02:10 +0000

    LeetCode

---
+++ solutions/2734. Lexicographically Smallest String After Substring Operation/2734.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  string smallestString(string s) {
+    const int n = s.length();
+    int i = 0;
+
+    while (i < n && s[i] == 'a')
+      ++i;
+    if (i == n)
+      s[n - 1] = 'z';
+
+    while (i < n && s[i] != 'a')
+      --s[i++];
+
+    return s;
+  }
+};
